d53427128d52cac39d051d0fd1fbe85c3e988a19
DESKTOP-RDFAH77\Trung <thanhtrungtran8888@gmail.com>
2023-05-16 23:44:21 +0700

    tabscroll update

--- tabscroll/script.js
+++ tabscroll/script.js
@@ -13,25 +13,55 @@ window.addEventListener('load', function () {
             [...tabItem].forEach((item) => item.classList.remove('active'));
             e.target.classList.add('active');
             let leftSpacing = e.target.offsetLeft;
+            console.log(leftSpacing);
             if (e.target.offsetLeft > tabOffsetLeft) { 
                 e.target.offsetLeft - tabOffsetLeft
             }
             tabList.scroll(leftSpacing / 2, 0);
+            if (tabList.scrollLeft >= tabScrollWidth) {
+                tabNext.classList.add('disabled');
+            }
+            if (tabList.scrollLeft > 0) {
+                tabPrev.classList.remove('disabled');
+            }
+            if (tabList.scrollLeft < tabScrollWidth) {
+                tabNext.classList.remove('disabled');
+            }
+            if (tabList.scrollLeft <= 0 ) {
+                tabPrev.classList.add('disabled');
+            }
         });
         tabList.addEventListener('wheel', (e) => {
             const delta = e.deltaY;
             tabList.scrollLeft += delta;
-        });
+            if (tabList.scrollLeft >= tabScrollWidth) {
+                tabNext.classList.add('disabled');
+            }
+            if (tabList.scrollLeft > 0) {
+                tabPrev.classList.remove('disabled');
+            }
+            if (tabList.scrollLeft < tabScrollWidth) {
+                tabNext.classList.remove('disabled');
+            }
+            if (tabList.scrollLeft <= 0 ) {
+                tabPrev.classList.add('disabled');
+            }
+        },{passive: true});
         tabNext.addEventListener('click', () => {
             tabList.scrollLeft += deltaScroll;
-             console.log(tabList.scrollLeft);
+            if (tabList.scrollLeft > 0) {
+                tabPrev.classList.remove('disabled');
+            }
             if (tabList.scrollLeft >= tabScrollWidth ) {
                 tabNext.classList.add('disabled');
+                
             }
         })
         tabPrev.addEventListener('click', () => {
             tabList.scrollLeft -= deltaScroll;
-             console.log(tabList.scrollLeft);
+            if (tabList.scrollLeft < tabScrollWidth) {
+                tabNext.classList.remove('disabled');
+            }
             if (tabList.scrollLeft <= 0 ) {
                 tabPrev.classList.add('disabled');
             }
