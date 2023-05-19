ac1d3378bad7cee535bac5e5281bcde100fc09b1
trungtran1211 <thanhtrungtran8888@gmail.com>
2023-05-19 17:39:03 +0700

    slider

--- slider/script.js
+++ slider/script.js
@@ -1,8 +1,53 @@
 window.addEventListener('load', function(){
-    const slider = document.querySelector(".slider");
     const sliderMain = document.querySelector(".slider-main");
-    const sliderItem = document.querySelectorAll(".slider-item");
+    const sliderItems = document.querySelectorAll(".slider-item");
     const nextBtn = document.querySelector(".slider-next");
     const prevBtn = document.querySelector(".slider-prev");
-    const dotItem = document.querySelectorAll(".slider-dot-item");
+    const dotItems = document.querySelectorAll(".slider-dot-item");
+
+    let positionX = 0;
+    let index = 0;
+    const sliderLength = sliderItems.length;
+    const sliderItemWidth = sliderItems[1].offsetWidth;
+    nextBtn.addEventListener("click", function() {
+        [...dotItems].forEach((item) => {item.classList.remove("active")});
+        item.classList.add("active");
+        handleChangeSlider(1);
+    });
+
+    prevBtn.addEventListener("click", function() {
+        handleChangeSlider(-1);
+    });
+
+    [...dotItems].forEach((item) => {
+        item.addEventListener("click", (e) => {
+            [...dotItems].forEach((el) => {el.classList.remove("active")});
+            item.classList.add("active");
+            const sliderIndex = parseInt(e.target.dataset.index);
+            index = sliderIndex;
+            positionX = -1 * index * sliderItemWidth;
+            sliderMain.style.transform = `translateX(${positionX}px)`;
+        });
+    });
+
+    handleChangeSlider = (direction) => {
+        if (direction === 1){
+            if (index < parseInt(sliderLength - 1)) {
+                index++;
+            }else {
+                return;
+            }
+            positionX = positionX - sliderItemWidth; 
+            sliderMain.style.transform = `translateX(${positionX}px)`;
+        }else if (direction === -1) {
+            if (index > 0) {
+                index--;
+            }else {
+                return;
+            }
+            positionX = positionX + sliderItemWidth; 
+            sliderMain.style.transform = `translateX(${positionX}px)`;
+        }
+    };
+
 });
